d3f1d47a3bc0227625cc619236382b936fdf016d
sean-code <john.nganga@student.moringaschool.com>
2022-11-09 01:57:49 +0300

    Add menu, edit and add UI effects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Carousels from './components/carousel/carousel';
+import Menu from './components/menu/menu';
 // import Carousel from './components/carousel/carousel';
 
 function App() {
   return (
     <>
       <Carousels />
+      <Menu />
     </>
   );
 }
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -2,9 +2,54 @@ import './menu.css'
 
 function Menu(){
     return(
-        <div>
+        <div className="container">
+            <h2 className="text-center">Food menu</h2>
+            <div className="food-menu-box">
+                <div className="img-food-menu">
+                    <img src="https://images.unsplash.com/photo-1523982440192-e61efd063acb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aXRhbGlhbiUyMGNoaWNrZW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt='food' className="img-order" />
+                </div>
+                <div className="Desc-food-menu">
+                        <h3 className="food-title">Food Title</h3>
+                        <p><b>Ksh 450</b> </p>
+                        <p className="para-formating">Made with italian Sauce, Chiken, and organice vegetables.<br/>
+                        <a href="#foods.com" className=" btn  order-now">Order Now</a>
+                        </p>
+                </div>
+                <div className="img-food-menu">
+                    <img src="https://images.unsplash.com/photo-1562158074-d16650a22f83?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fENoaW5lZXNlJTIwU3VzaGl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt='food2' className="img-order" />
+                </div>
+                <div className="Desc-food-menu">
+                        <h3 className="food-title">Food Title</h3>
+                        <p><b>Ksh 700</b> </p>
+                        <p className="para-formating">Made with italian Sauce, Chiken, and organice vegetables.<br/>
+                        <a href="#foods.com" className=" btn  order-now">Order Now</a>
+                        </p>
+                </div>
+                <div className="img-food-menu">
+                    <img src="https://images.unsplash.com/photo-1562158074-d16650a22f83?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fENoaW5lZXNlJTIwU3VzaGl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt='food2' className="img-order" />
+                </div>
+                <div className="Desc-food-menu">
+                        <h3 className="food-title">Food Title</h3>
+                        <p><b>Ksh 100</b> </p>
+                        <p className="para-formating">Made with italian Sauce, Chiken, and organice vegetables.<br/>
+                        <a href="#foods.com" className=" btn  order-now">Order Now</a>
+                        </p>
+                </div>/
+                <div className="img-food-menu">
+                    <img src="https://images.unsplash.com/photo-1562158074-d16650a22f83?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fENoaW5lZXNlJTIwU3VzaGl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt='food2' className="img-order" />
+                </div>
+                <div className="Desc-food-menu">
+                        <h3 className="food-title">Food Title</h3>
+                        <p><b>Ksh 500</b> </p>
+                        <p className="para-formating">Made with italian Sauce, Chiken, and organice vegetables.<br/>
+                        <a href="#foods.com" className=" btn  order-now">Order Now</a>
+                        </p>
+                </div>
             
+            </div>
         </div>
+        
+
     )
 }
 
