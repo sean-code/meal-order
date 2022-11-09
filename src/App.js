699f48ce2f3c107e3118aeb2cb97f1e5c6478c18
codingvaleria <codingvaleria@gmail.com>
2022-11-09 18:34:47 +0300

    weird files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import React from "react";
+import Menu from "./components/menu/menu";
+import "./components/menu/menu.css";
 
 function App() {
   return (
-    <>
-
-    </>
+    <div>
+      <Menu />
+    </div>
   );
 }
 
