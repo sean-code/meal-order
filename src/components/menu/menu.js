1abbfc2381fab48252c60da47312d11c898a3fc5
codingvaleria <codingvaleria@gmail.com>
2022-11-09 20:26:41 +0300

    fetch and set state for menu component

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,159 +1,24 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Meal from "./meal";
 
 function Menu() {
-  return (
-    <div className="Menu">
-      <section className="food-menu">
-        <div className="container">
-          <h2 className="text-center">Food menu</h2>
-          {/* <!--********* Box-1 ***************--> */}
-          <div className="food-menu-box">
-            <div className="img-food-menu">
-              <img
-                src="https://i.postimg.cc/L6VKJBys/menu-pizza.jpg"
-                alt="food menu Image"
-                className="img-order"
-              />
-            </div>
-            <div className="Desc-food-menu">
-              <h3 className="food-title">Food Title</h3>
-              <p>
-                <b>$2.3</b>
-              </p>
-              <p className="para-formating">
-                Made with italian Sauce, Chiken, and organice vegetables.
-                <br />
-                <br />
-                <a href="" className="btn order-now">
-                  Order Now
-                </a>
-              </p>
-            </div>
-          </div>
-          {/* <!--********* Box-2 ***************--> */}
-          <div className="food-menu-box">
-            <div className="img-food-menu">
-              <img
-                src="https://i.postimg.cc/j2HVQztZ/menu-burger.jpg"
-                alt="food menu Image"
-                className="img-order"
-              />
-            </div>
-            <div className="Desc-food-menu">
-              <h3 className="food-title">Food Title</h3>
-              <p>
-                <b>$2.0</b>
-              </p>
-              <p className="para-formating">
-                Made with italian Sauce, Chiken, and organice vegetables.
-                <br />
-                <br />
-                <a href="" className="btn order-now">
-                  Order Now
-                </a>
-              </p>
-            </div>
-          </div>
+  const [meals, setMeals] = useState([]);
+  const baseUrl = "http://localhost:8000";
 
-          <div className="food-menu-box">
-            {/* <!--********* Box-3 ***************--> */}
-            <div className="img-food-menu">
-              <img
-                src="https://i.postimg.cc/66nNXNnn/menu-momo.jpg"
-                alt="food menu Image"
-                className="img-order"
-              />
-            </div>
-            <div className="Desc-food-menu">
-              <h3 className="food-title">Food Title</h3>
-              <p>
-                <b>$1.5</b>
-              </p>
-              <p className="para-formating">
-                Made with italian Sauce, Chiken, and organice vegetables.
-                <br />
-                <br />
-                <a href="" className="btn order-now">
-                  Order Now
-                </a>
-              </p>
-            </div>
-          </div>
-          {/* <!--********* Box-4 ***************--> */}
-          <div className="food-menu-box">
-            <div className="img-food-menu">
-              <img
-                src="https://i.postimg.cc/L6VKJBys/menu-pizza.jpg"
-                alt="food menu Image"
-                className="img-order"
-              />
-            </div>
-            <div className="Desc-food-menu">
-              <h3 className="food-title">Food Title</h3>
-              <p>
-                <b>$2.3</b>
-              </p>
-              <p className="para-formating">
-                Made with italian Sauce, Chiken, and organice vegetables.
-                <br />
-                <br />
-                <a href="" className="btn order-now">
-                  Order Now
-                </a>
-              </p>
-            </div>
-          </div>
-          {/* <!--********* Box-5 ***************--> */}
-          <div className="food-menu-box">
-            <div className="img-food-menu">
-              <img
-                src="https://i.postimg.cc/j2HVQztZ/menu-burger.jpg"
-                alt="food menu Image"
-                className="img-order"
-              />
-            </div>
-            <div className="Desc-food-menu">
-              <h3 className="food-title">Food Title</h3>
-              <p>
-                <b>$2.0</b>
-              </p>
-              <p className="para-formating">
-                Made with italian Sauce, Chiken, and organice vegetables.
-                <br />
-                <br />
-                <a href="" className="btn order-now">
-                  Order Now
-                </a>
-              </p>
-            </div>
-          </div>
-          <div className="food-menu-box">
-            <div className="img-food-menu">
-              <img
-                src="https://i.postimg.cc/j2HVQztZ/menu-burger.jpg"
-                alt="food menu Image"
-                className="img-order"
-              />
-            </div>
-            <div className="Desc-food-menu">
-              <h3 className="food-title">Food Title</h3>
-              <p>
-                <b>$2.0</b>
-              </p>
-              <p className="para-formating">
-                Made with italian Sauce, Chiken, and organice vegetables.
-                <br />
-                <br />
-                <a href="" className="btn order-now">
-                  Order Now
-                </a>
-              </p>
-            </div>
-          </div>
-          <div className="clearfix"></div>
-        </div>
-      </section>
-    </div>
+  useEffect(() => {
+    fetch(`${baseUrl}/meals`)
+      .then((res) => res.json())
+      .then((meals) => setMeals(meals));
+  });
+
+  return (
+    <>
+      {" "}
+      {meals.map((meal) => (
+        <Meal key={meal.id} meal={meal} />
+      ))}{" "}
+    </>
   );
 }
 
