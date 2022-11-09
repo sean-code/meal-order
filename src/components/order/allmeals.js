43f7ed37350ac9948510c16c6b9ebe9e48971827
krisine <kristine.nyaga@student.moringaschool.com>
2022-11-09 10:16:37 +0300

    gets  meals from the api endpoint

---
+++ src/components/order/allmeals.js
@@ -0,0 +1,26 @@
+import React, { useEffect,useState } from "react";
+import Cards from "./card";
+import "./allmeals.css";
+
+const Allmeals = ({handleClick}) => {
+  const baseUrl ="http://localhost:8000";
+  const [meals,setMyMeals]=useState([])
+  useEffect(()=>{
+    fetch(`${baseUrl}/meals`)
+    .then(response => response.json())
+    .then(response => setMyMeals(response))
+    .catch(error =>alert(error) )
+
+  },[])
+  console.log(meals)
+  
+  return (
+    <section>
+      {meals.map((item) => (
+        <Cards key={item.id} item={item} handleClick={handleClick} />
+      ))}
+    </section>
+  );
+};
+
+export default Allmeals;
