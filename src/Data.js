dae0a98d8e91eb4f400deb8234b73315f837b241
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-11-10 05:30:13 +0300

    add Data.js

---
+++ src/Data.js
@@ -0,0 +1,24 @@
+const Slides = [
+    {
+      url: "shorturl.at/eiA24",
+      text: "Kenyan Kachumbari Salad with Fish and Ugali"
+    },
+    {
+      url:"shorturl.at/gjmZ1",
+      text: "steak with rustic fried potatoes with onions, cherry tomatoes"
+    },
+    {
+      url: "shorturl.at/fhjN0",
+      text: "Pilau with beef meat close Up selective Focus"
+    },
+    {
+        url: "shorturl.at/nwy15",
+        text: "Fried Chicken"
+      },
+    {
+      url: "shorturl.at/mJR27",
+      text: "Swahili Nyama Choma"
+    }  
+]
+
+export {Slides}
