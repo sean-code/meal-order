fe435d92e5642226cf605c41db5ef7629271d206
krisine <kristine.nyaga@student.moringaschool.com>
2022-11-10 07:09:11 +0300

    adding the routes to the checkout

--- src/App.js
+++ src/App.js
@@ -1,37 +1,74 @@
 import './App.css';
 import Allmeals from './components/order/allmeals';
 import Cart from './components/order/cart';
-import Order from './components/order/order'
+import Order from './components/order/order';
 import React,{useState,useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
-  const [show,setShow] = useState(true)
-  const [cart, setCart] = useState([]);
+  const [show, setShow] = useState(true);
+	const [cart , setCart] = useState([]);
+	const [warning, setWarning] = useState(false);
+  const [amount,setAmount] = useState(1)
+  
+  useEffect(()=>{
 
-  const handleClick = (item) => {
-    console.log(item)
-    if (cart.indexOf(item) !== -1) return;
-    setCart([...cart, item]);
-  };
-  const handleChange = (item, d) => {
-    const ind = cart.indexOf(item);
-    const arr = cart;
-    arr[ind].amount += d;
+  })
 
-    if (arr[ind].amount === 0) arr[ind].amount = 1;
-    setCart([...arr]);
-  };
-  console.log(cart)
+  console.log(amount)
+	const handleClick = (item)=>{
+    console.log(item.id)
+		let isPresent = false;
+		cart.forEach((product)=>{
+			if (item.id === product.id)
+			isPresent = true;
+		})
+		if (isPresent){
+      console.log("there")
+			setWarning(true);
+			setTimeout(()=>{
+				setWarning(false);
+			}, 2000);
+			return ;
+		}
+		setCart([...cart, item]);
+	}
+
+	const handleChange = (item, amount,d) =>{
+    console.log(amount,item)
+		let ind = -1;
+    amount = amount++
+    setAmount(amount)
+		cart.forEach((data, index)=>{
+			if (data.id === item.id)
+				ind = index;
+		});
+		const tempArr = cart;
+		tempArr[ind].amount += amount;
+		
+		if (tempArr[ind].amount === 0)
+			tempArr[ind].amount = 1;
+		setCart([...tempArr])
+	}
+  
   return (
-    <>
-      <Order setShow={setShow}/>
-      {
-        show? <Allmeals handleClick={handleClick}/> : <Cart setCart={setCart} cart={cart}  handleChange={handleChange}/>      }
-      
-      {/* <Cart cart={cart} setCart={setCart} />   */}
-
-    </>
+    <React.Fragment>
+       
+		<Order size={cart.length} setShow={setShow} />
+		{
+			show ? <Allmeals handleClick={handleClick} /> : <Cart cart={cart} setCart={setCart} handleChange={handleChange} amount={amount} />
+		}
+		{
+			warning && <div className='warning'>Item is already added to your cart</div>
+		}
+    <Router>
+      <Routes>
+       <Route exact path="/meal-order/src/components/order/cart.js" element={<Cart cart={cart} setCart={setCart} handleChange={handleChange} amount={amount} />} />
+             
+      </Routes>
+  </Router>
+	</React.Fragment>
   );
 }
 
