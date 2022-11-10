13e8aff78c8c8cdb45278e09effece06a2ef8a73
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-11-10 16:26:23 +0300

    add slideshow images

--- src/components/Data.js
+++ src/components/Data.js
@@ -4,13 +4,18 @@ const Slides = [
       text: "Dish A"
     },
     {
-      url:"https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      url:"https://images.unsplash.com/photo-1594998893017-36147cbcae05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1186&q=80",
       text: "Dish B"
     },
     {
-      url: "",
-      text: "DESKTOP SALES"
+      url: "https://media.istockphoto.com/id/1030110292/photo/pilaf-pilau-or-plov-with-beef-meat.jpg?s=612x612&w=is&k=20&c=gYOXpsv73gONyXF-dpoW-yQ0618EVG9-HkDUaY3eG8s=",
+      text: "Dish C"
+    },
+    {
+        url:"https://media.istockphoto.com/id/1175505781/photo/arabic-and-middle-eastern-dinner-table-hummus-tabbouleh-salad-fattoush-salad-pita-meat-kebab.jpg?s=612x612&w=is&k=20&c=0PU48nRl0kOnPCFDZCNUKiXQ97cc-qWrXL1wVS7p6ZY=",
+        text:"Dish D"
     }
+
 ]
 
 export {Slides}
