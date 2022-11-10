4bd1f06dfc56665be55e0c9d43f50c9358d291ba
krisine <kristine.nyaga@student.moringaschool.com>
2022-11-10 07:12:02 +0300

    adds show functionality

--- src/components/order/order.js
+++ src/components/order/order.js
@@ -1,22 +1,22 @@
 import './order.css'
 
- function Order ({setShow}){
+ function Order ({setShow,size}){
     return(
         
-              <nav>
-                <div className="nav_box">
-                  <span className="my_order" onClick={setShow(true)}>
-                    My Orders
-                  </span>
-                  <div className="cart" onClick={setShow(false)}>
-                    <span>
-                      <i className="fas fa-cart-plus"></i>
-                    </span>
-                    <span>0</span>
-                  </div>
-                </div>
-              </nav>
-            );
+    <nav>
+      <div className="nav_box">
+        <span className="my_order" onClick={()=>setShow(true)}>
+          My Orders
+        </span>
+        <div className="cart" onClick={()=>setShow(false)}>
+          <span>
+            <i className="fas fa-cart-plus"></i>
+          </span>
+          <span>{size}</span>
+        </div>
+      </div>
+    </nav>
+  );
         
     
 };
