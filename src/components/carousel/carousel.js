c5ce6c3bdc90d73c9c5e006b7f782e78389ebe62
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-11-10 05:26:38 +0300

    add carousel.js

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,11 +1,43 @@
-import './carousel.css'
+import React, { useEffect, useRef, useState} from 'react'
+import { Slides as slides } from '../Data';
+import SlideDots from './SlideDots';
 
-function Carousel(){
-    return(
-        <div>
-            
-        </div>
-    )
+
+export default function Carousel() {
+    const [index, setIndex] = useState(0);
+    const delay = 5000;
+    const timeoutRef = useRef(null);
+    function resetTimeout() {
+        if (timeoutRef.current) {
+          clearTimeout(timeoutRef.current);
+        }
+    }
+    useEffect(() => {
+      resetTimeout();
+      timeoutRef.current = setTimeout(
+        () =>
+          setIndex((prevIndex) =>
+            prevIndex === slides.length - 1 ? 0 : prevIndex + 1
+          ),
+        delay
+      );
+      return () => {
+        resetTimeout();
+      };
+    }, [index]);
+
+  return (
+    <div className='carousel-container'>
+      <div className='mySlides fade' style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
+        {slides.map((item, index) => 
+          <div key={index} className="slider">
+            <img src={item.url} alt="" style={{width: "100%"}}/>
+            <div className='text'>{item.text}</div>
+          </div> 
+        )}                   
+      </div>
+      <SlideDots slides={slides} index={index} setIndex={setIndex}/>
+    </div>   
+  )
 }
-
 export default Carousel;
