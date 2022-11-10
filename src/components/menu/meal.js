8a8a6f10cb370930a7088b4d317e696e4472a432
codingvaleria <codingvaleria@gmail.com>
2022-11-10 21:13:54 +0300

    update classes

--- src/components/menu/meal.js
+++ src/components/menu/meal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./menu.css";
 
 function Meal({ meal }) {
   const { name, cuisine, price, image } = meal;
@@ -6,19 +7,19 @@ function Meal({ meal }) {
   return (
     <div className="food-menu-box">
       <div className="img-food-menu">
-        <img src={image} alt="food menu Image" className="img-order" />
+        <img src={image} alt="food menu" className="img-order" />
       </div>
 
       <div className="Desc-food-menu">
-        <span>
-          <h3 className="food-title">{name}</h3>
+        <span className="food-info">
+          <span className="food-title">{name}</span>
           <span> {cuisine}</span>
         </span>
 
-        <p className="para-formating">
-          <p>Ksh: {price}</p>
-          <button className="btn order-now"> Order Now</button>
-        </p>
+        <span className="para-formating">
+          <span className="price"> Ksh:{price}</span>
+          <button className="btn-order-now"> Order Now</button>
+        </span>
       </div>
     </div>
   );
